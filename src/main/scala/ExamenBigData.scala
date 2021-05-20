47bf23fc08a0769c2ed2b22601bd45c9fae5f958
RickiTraore <siriki.traore20inphb.ci>
2021-05-20 12:46:28 +0000

    ajout de la fonction liste_double() qui est fait à moitié.

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -6,9 +6,33 @@ object ExamenBigData {
     return  liste2
   }
 
+  def liste_double() : Unit ={
+    val tab : Map[String,String] = Map(
+      "ecommercemag.fr" -> "",
+      "https://www.journalducm.com/contact/" -> "Payant",
+      "https://www.zatsaz.com/" -> "",
+      "https://www.lerevenu.com/" -> "",
+      "https://www.cadre-dirigfdeant-magazine.com/" -> "Payant",
+      "https://www.silicon.fr/services/contact#annoncer" -> "Payant",
+      "https://www.channelbiz.fr/nous-contacter/" -> "",
+      "https://www.itespresso.fr/" -> "",
+      "https://www.industrie-mag.com/article4.html/" -> "invité",
+      "https://www.jesuissundev.com/article-invite/" -> "invité",
+      "https://www.numerama.com/" -> ""
+    )
+
+    val secondeTab  = tab.map(l => {
+      if (l._1.startsWith("https://www.")){ (l._1.substring(12),l._2) }
+      else{ (l._1, l._2)}
+    }).foreach(l =>println(l))
+  }
+
   def main(args: Array[String]): Unit = {
     // test de la fonction
     val liste = retournListe(List("Julien", "Paul", "Jean", "rac", "trec", "joel", "ed", "chris", "maurice"))
     liste.foreach(l =>println(l))
+    liste_double()
+
+
   }
 }
